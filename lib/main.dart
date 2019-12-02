47504b54aaa7cf690f0be9003818a98aec91a72c
hibikiNPC <hibiki.fox@gmail.com>
2019-12-02 16:58:20 +0900

    homeに追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -213,12 +214,41 @@ class Home extends StatelessWidget {
         ], 
       ),
       body: new Center(
-        child: new RaisedButton(
-          child: const Text("設定"),
-          onPressed: () {
-            // 設定画面へ
-            Navigator.of(context).pushNamed("/setting");
-          },
+        child: new Column(
+          children: <Widget>[
+
+            Padding(
+              padding: EdgeInsets.only(top: 70.0, bottom: 10.0),
+              child: const Text('温度',
+              style: TextStyle(fontSize: 20)),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 30.0),
+              child: const Text('20℃',
+              style: TextStyle(fontSize: 40)),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+              child: const Text('湿度',
+              style: TextStyle(fontSize: 20)),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 40.0),
+              child: const Text('50%',
+              style: TextStyle(fontSize: 40)),
+            ),
+
+            new RaisedButton(
+            child: const Text("設定"),
+            onPressed: () {
+              // 設定画面へ
+              Navigator.of(context).pushNamed("/setting");
+              },
+            ),
+          ]
         ),
       ),
     );
